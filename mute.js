fd26ea83e9f7f030dd18bc4b401c146e41e56463
John Garcia <166593810+johnalvarogarcia@users.noreply.github.com>
2024-04-13 17:19:20 +1200

    Create mute.js

---
+++ mute.js
@@ -0,0 +1,8 @@
+
+sf.ui.app('com.uaudio.ua_mixer_engine').getElement('AXFocusedUIElement').mouseClickElement({
+    relativePosition: {"x":-25,"y":-200},
+    anchor: "BottomRight",
+});
+
+sf.ui.proTools.appActivateMainWindow();
+
